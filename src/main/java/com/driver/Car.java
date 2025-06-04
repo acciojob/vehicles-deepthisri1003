36ab670391680fbcef107855e80602cedee1f96c
deepthisri1003 <dsrvundela@gmail.com>
2025-06-04 21:49:02 +0530

    Vehicle assignment got submitted

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -6,11 +6,76 @@ public class Car extends Vehicle {
     private int doors;
     private int gears;
     private boolean isManual;
+
+
+    public int wheels(){
+        return wheels;
+    }
+    public void setWheels(int wheels){
+        this.wheels = wheels;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
     private int currentGear;
     private int seats;
 
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.type = type;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.seats = seats;
+        this.currentGear =1;
     }
 
     public void changeGear(int newGear){
